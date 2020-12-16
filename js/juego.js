53241b40cb8ef9bfc2df64f387951c042d23c189
AndreeaCCA <andreeaccandreea@gmail.com>
2020-12-16 19:06:24 +0100

    cambios Rosa

--- js/juego.js
+++ js/juego.js
@@ -1,32 +1,56 @@
-const cards = [
-    { value: "css", img: "images/cards/css.png" },
-    { value: "gitlab", img: "images/cards/gitlab.png " },
-    { value: "html", img: "images/cards/html.png" },
-    { value: "internet", img: "images/cards/internet.png" },
-    { value: "js", img: "images/cards/js.png" },
-    { value: "mochuelo", img: "images/cards/mochuelo.png" },
-    { value: "php", img: "images/cards/php.png" },
-    { value: "python", img: "images/cards/python.png" },
-    { value: "react", img: "images/cards/react.png" },
-    { value: "sass", img: "images/cards/sass.png" }]
+  let cards = [
+    { id: "0", img: "images/cards/css.png" },
+    { id: "1", img: "images/cards/gitlab.png " },
+    { id: "2", img: "images/cards/html.png" },
+    { id: "3", img: "images/cards/internet.png" },
+    { id: "4", img: "images/cards/js.png" },
+    { id: "5", img: "images/cards/mochuelo.png" },
+    { id: "6", img: "images/cards/php.png" },
+    { id: "7", img: "images/cards/python.png" },
+    { id: "8", img: "images/cards/react.png" },
+    { id: "9", img: "images/cards/sass.png" }]
 
-const cards2 = [
-    { value: "css", img: "images/cards/css.png" },
-    { value: "gitlab", img: "images/cards/gitlab.png " },
-    { value: "html", img: "images/cards/html.png" },
-    { value: "internet", img: "images/cards/internet.png" },
-    { value: "js", img: "images/cards/js.png" },
-    { value: "mochuelo", img: "images/cards/mochuelo.png" },
-    { value: "php", img: "images/cards/php.png" },
-    { value: "python", img: "images/cards/python.png" },
-    { value: "react", img: "images/cards/react.png" },
-    { value: "sass", img: "images/cards/sass.png" }]
-
-
+  let cards2 = [
+        { id: "0", img: "images/cards/css.png" },
+        { id: "1", img: "images/cards/gitlab.png " },
+        { id: "2", img: "images/cards/html.png" },
+        { id: "3", img: "images/cards/internet.png" },
+        { id: "4", img: "images/cards/js.png" },
+        { id: "5", img: "images/cards/mochuelo.png" },
+        { id: "6", img: "images/cards/php.png" },
+        { id: "7", img: "images/cards/python.png" },
+        { id: "8", img: "images/cards/react.png" },
+        { id: "9", img: "images/cards/sass.png" }]
 
 
 
-function crearCartas(num) {
+let carta= document.querySelector("#carta")
+let tablero = document.getElementById("tablero")
+
+   
+
+
+
+function mostrarCartas() {
+    //primero desordenamos el array cards:
+    //cards.sort(e => Math.random() - 0.5)
+    
+    //ahora lo recorremos para crear una carta y mostrarla:
+    for (let i = 0; i < cards.length; i++) {
+        let duplicado = carta.cloneNode(true)
+        duplicado.id = "carta-" + i
+        duplicado.style.display = "block"
+        tablero.appendChild(duplicado) 
+        
+    }
+}
+    mostrarCartas()
+
+    
+
+
+
+/* function crearCartas(num) {
     for (i = 0; i < num; i++) {
         let carta = document.createElement("div")
         let back = carta.appendChild(document.createElement("img"))
@@ -38,17 +62,28 @@ function crearCartas(num) {
         document.querySelector(".wrapper").appendChild(carta)
     }
 }
+ */
+let cartaLevantada=0
+let indiceCartaLevantada
+
+document.querySelector("#tablero").onclick=(e)=>{
+    if(e.target.className=="cartita"){
+        if(cartaLevantada==0){ 
+        e.target.src=`${cards1[e.target.dataset.id].img}`
+        cartaLevantada=1
+        indeceCartaLevantada=cards[e.target.dataset.id]}
+
+    }else if(cartaLevantada==1){
+        if(cards[indeceCartaLevantada].img==cards2[e.target.dataset.id].img){
+            //has acertado
+        }
+    }
+}
 
 
-document.querySelectorAll(".front")
-
-if (contactos[i].gender == "f")
-    duplicado.querySelector("[name='gender']").src = "../images/f.png"
-listado.du
 
 
 
-crearCartas(20)
 
 
 // OCULTAR ESTRELLAS
